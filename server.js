2d435dd933ea5043715d9a98f227c1c0c9974c67
Saiyam <jindal.saiyam123@gmail.com>
2020-07-22 18:36:51 +0530

    hpo

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+app.use(express.static('public'));
+app.get("*",function(req,res){
+    res.end("<h1>response from express server</h1>");
+})
+const port = process.env.PORT || 3000;
+app.listen(port,function(){
+    console.log("server is listening");
+})
